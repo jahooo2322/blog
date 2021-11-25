150d99f316244bd13c78d341b4bf15e429acefe0
jahooo2322 <jailsonbv8@gmail.com>
2021-11-24 21:10:29 -0300

    teste link

---
+++ pages/sobre.js
@@ -0,0 +1,20 @@
+//import {useState} from 'react'
+
+import Link from 'next-link'
+
+function Sobre(){
+
+    return(
+<div>
+     <h1>Home</h1>
+     <Link href="/">
+     <a>Sobre</a> 
+     </Link>
+</div>
+
+    )
+}
+
+
+
+export default Sobre
